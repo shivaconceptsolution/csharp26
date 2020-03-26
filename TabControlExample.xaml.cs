046a61eccdeb575b3f0cab4b95ee1862cd18a218
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2020-03-26 16:00:08 +0530

    Add files via upload

---
+++ TabControlExample.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfControlExample
+{
+    /// <summary>
+    /// Interaction logic for TabControlExample.xaml
+    /// </summary>
+    public partial class TabControlExample : Window
+    {
+        public TabControlExample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPreviousTab_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tcSample.SelectedIndex - 1;
+            if (newIndex < 0)
+                newIndex = tcSample.Items.Count - 1;
+            tcSample.SelectedIndex = newIndex;
+        }
+
+        private void btnNextTab_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tcSample.SelectedIndex + 1;
+            if (newIndex >= tcSample.Items.Count)
+                newIndex = 0;
+            tcSample.SelectedIndex = newIndex;
+        }
+
+        private void btnSelectedTab_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Selected tab: " + (tcSample.SelectedItem as TabItem).Header);
+        }
+
+    }
+}
---
+++ WindowFormHosting.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfControlExample
+{
+    /// <summary>
+    /// Interaction logic for WindowFormHosting.xaml
+    /// </summary>
+    public partial class WindowFormHosting : Window
+    {
+        public WindowFormHosting()
+        {
+            InitializeComponent();
+            (wfhSample.Child as System.Windows.Forms.WebBrowser).Navigate("http://www.shivatutorials.com");
+        }
+        private void wbWinForms_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            this.Title = (sender as System.Windows.Forms.WebBrowser).DocumentTitle;
+        }
+
+    }
+}
